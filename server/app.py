b33e19d113dc93dccf669f167ce0177892ee1d87
Derek Truesdale <dtruesdale37@gmail.com>
2023-09-06 13:49:02 -0500

    lab completed

--- server/app.py
+++ server/app.py
@@ -4,5 +4,34 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<string:parameter>')
+def print_string(parameter):
+      print(parameter)
+      return parameter
+
+@app.route('/count/<int:parameter>')
+def count_numbers(parameter):
+    numbers= '\n'.join(str(i) for i in range(parameter))
+    return numbers
+
+@app.route('/math/<int:num1>/<string:operation>/<int:num2>')
+def math_operation(num1, operation, num2):
+    if operation == '+':
+        total =num1 + num2
+    elif operation == '-':
+        total = num1 - num2
+    elif operation == '*':
+        total = num1 * num2
+    elif operation == '/' or operation == 'div':
+        total = num1 / num2
+    elif operation == '%':
+        total = num1 % num2
+    return str(total)
+
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/testing/app_test.py
+++ server/testing/app_test.py
@@ -40,9 +40,9 @@ class TestApp:
         assert(response.status_code == 200)
 
     def test_count_range_10(self):
-        '''counts through range of parameter in "/count/<parameter" on separate lines.'''
+        '''counts through range of parameter in "/count/<parameter>" on separate lines.'''
         response = app.test_client().get('/count/10')
-        count = '0\n1\n2\n3\n4\n5\n6\n7\n8\n9\n'
+        count = '0\n1\n2\n3\n4\n5\n6\n7\n8\n9'
         assert(response.data.decode() == count)
 
     def test_math_route(self):
